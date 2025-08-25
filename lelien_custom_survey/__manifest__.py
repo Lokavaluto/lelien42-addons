724a0f8c06a0a1d8bb9582d23b46a544d89cf292
Laetitia Da Costa <laetitia.dacosta@elabore.coop>
2025-08-25 13:42:50 +0200

    [ADD]lelien_custom_survey

---
+++ lelien_custom_survey/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'lelien_custom_survey',
+    'version': '16.0.1.0.0',
+    'author': 'Elabore',
+    'description': "Le Lien 42 : various customizations for surveys",
+    'depends': ['base','survey'],
+    'data': [
+    ],
+    'license': 'LGPL-3',
+    'installable': True,
+    "auto_install": False,
+    "application": False,
+}
